36ba0ad616d5be2ad389dea5dee470b71c08a8a7
lqchel <liangqianchen@hotmail.com>
2020-12-09 16:08:18 +1100

    marking critical object v1

--- massive_analysis.m
+++ massive_analysis.m
@@ -22,72 +22,40 @@ Find_Incongruent_IP = Results(:,4) == 1 & Results(:,5) == 3 & Results(:,6) == 3;
 Find_Congruent_IP = Results(:,4) == 0 & Results(:,5) == 3 & Results(:,6) == 3;
 Find_Incongruent_CP = Results(:,4) == 1 & Results(:,5) == 2 & Results(:,6) == 3;
 
+location1 = (Results(:,7)==2 | Results(:,7)==4| Results(:,7)==6|Results(:,7)== 8) .* 6.5;
+location2 = (Results(:,7)==1 | Results(:,7)==3| Results(:,7)==7|Results(:,7)== 9) .* 9.2;
+eccentricity = zeros(length(Results),1)+ location1 + location2;
+
+
 %% original vs. modified
 
 %congruent
-delta_1 = zeros(80,2);
+delta_1 = zeros(80,3);
 for img = 3:82
     delta_1(img-2,1) = median(Results(Results(:,2)==img & Find_Congruent_CP,9)-Results(Results(:,2)==img & Find_Congruent_IP,9));
     delta_1(img-2,2) = img;
+    delta_1(img-2,3) = unique(eccentricity(Results(:,2)==img & Find_Congruent_CP,1));
+    c(img-2,1) = unique(Results(Results(:,2)==img & Find_Congruent_CP,7));
 end
 
 %incongruent
-
-delta_2 = zeros(80,2);
+delta_2 = zeros(80,3);
 for img = 3:82
     delta_2(img-2,1) = median(Results(Results(:,2)==img & Find_Incongruent_IP,9)-Results(Results(:,2)==img & Find_Incongruent_CP,9));
     delta_2(img-2,2) = img;
+    delta_2(img-2,3) = unique(eccentricity(Results(:,2)==img & Find_Incongruent_IP,1));
+    in(img-2,1) = unique(Results(Results(:,2)==img & Find_Incongruent_IP,7));
 end
 
 [delta_1_x,rank1] = sort(delta_1(:,1),'descend');
 [delta_2_x,rank2] = sort(delta_2(:,1),'descend');
 
-%% present vs.null
-d = importdata('FourOrFive.mat');
-delta_3 = zeros(80,2);
-loc_even = [2 4 6 8];
-loc_odd = [1 3 5 7 9];
+delta_1_x = [delta_1_x delta_1(rank1,2) delta_1(rank1,3)];
+delta_2_x = [delta_2_x delta_2(rank2,2) delta_2(rank2,3)];
 
-for img = 3:82
-    
-if d(img-2) == 0
-    for loc1 = 1:4
-        present = Results(Results(:,2)==img & Results(:,5) ~= 1 & Results(:,7) == loc_even(loc1),9);
-        if sum(Results(:,2)==img & Find_N & Results(:,7) == loc_even(loc1)) ~= 0
-            null = Results(Results(:,2)==img & Find_N & Results(:,7) == loc_even(loc1),9);
-            for i = 1:size(null,1)
-                dc_diff(i) = median(present) - null(i,:);
-            end
-        else
-            loc1 = loc1+1;
-        end   
-        
-    end
-    delta_3(img-2,1) = median(dc_diff); 
-    delta_3(img-2,2) = img;
-else
-    for loc2 = 1:5
-        present = Results(Results(:,2)==img & Results(:,5) ~= 1 & Results(:,7) == loc_odd(loc2),9);
-        if sum(Results(:,2)==img & Find_N & Results(:,7) == loc_odd(loc2)) ~= 0
-            null = Results(Results(:,2)==img & Find_N & Results(:,7) == loc_odd(loc2),9);
-            for a = 1:size(null,1)
-                dc_diff(a) = median(present) - null(a,:);
-            end
-        else
-            loc2 = loc2+1;
-        end   
-        
-    end
-    delta_3(img-2,1) = median(dc_diff); 
-    delta_3(img-2,2) = img;
-    
-end
-clear present
-clear null
-
-end
-
-%% plotting
+delta_1_x = [delta_1_x(:,1:3) c(rank1,1)];
+delta_2_x = [delta_2_x(:,1:3) in(rank2,1)];
+%% plotting the lines
 colours = cbrewer('qual', 'Set1', 8);
 [Y1,edges] = histcounts(delta_1(:,1),80);
 Y1_cumulative  = cumsum(Y1);
@@ -106,12 +74,147 @@ set(gca,'FontName','Arial','FontSize',12);
 xlim([-7 7]);
 legend({'Congruent','Incongruent'},'Box','off','Location','northwest');
 
-[Y3,edges] = histcounts(delta_3(:,1),80);
-Y3_cumulative  = cumsum(Y3);
-subplot(2,1,2),plot(edges,[Y3_cumulative 80],'LineWidth',1.2,'Color','magenta');
+%% display images based on delta rcBxC
+% load image list
+folder1 = 'C:\Users\liang\OneDrive\Documents\honours\research project\MassiveReport_Exp2_QL\squareimage\congruent cropped'; 
+folder2 = 'C:\Users\liang\OneDrive\Documents\honours\research project\MassiveReport_Exp2_QL\squareimage\incongruent cropped';
+filePattern1 = fullfile(folder1,'*.jpg');
+filePattern2 = fullfile(folder2,'*.jpg');
+theFiles1 = dir(filePattern1);
+theFiles2 = dir(filePattern2);
 
-xlabel('?rcD×C (Present - Null)'), ylabel('Cumulative counts');
+top = 1:3;
+mid_1 = 47:50;
+mid_2 = 97:100;
+bottom = 147:150;
+
+% display (axis definition [left bottom width height])
+
+%congruent
+for i = 1:1
+for p = 1:5
+    current_cong= imresize(imread(fullfile(folder1,theFiles1(delta_1_x((i-1).*5+p,2)).name)),[150 150]);
+    if delta_1_x((i-1).*5+p,4) == 1
+        current_cong([top mid_1],[top mid_1],1) = uint8(255);
+        current_cong([top mid_1],[top mid_1],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 2
+        current_cong([top mid_1],[mid_1 mid_2],1) = uint8(255);
+        current_cong([top mid_1],[mid_1 mid_2],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 3
+        current_cong([top mid_1],[mid_2 bottom],1) = uint8(255);
+        current_cong([top mid_1],[mid_1 bottom],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 4
+        current_cong([mid_1 mid_2],[top mid_1],1) = uint8(255);
+        current_cong([mid_1 mid_2],[top mid_1],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 5
+        current_cong([mid_1 mid_2],[mid_1 mid_2],1) = uint8(255);
+        current_cong([mid_1 mid_2],[mid_1 mid_2],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 6
+        current_cong([mid_1 mid_2],[mid_2 bottom],1) = uint8(255);
+        current_cong([mid_1 mid_2],[mid_2 bottom],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 7
+        current_cong([mid_2 bottom],[top mid_1],1) = uint8(255);
+        current_cong([mid_2 bottom],[top mid_1],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 8
+        current_cong([mid_2 bottom],[mid_1 mid_2],1) = uint8(255);
+        current_cong([mid_2 bottom],[mid_1 mid_2],2:3) = uint8(0);
+    else
+        current_cong([mid_2 bottom],[mid_2 bottom],1) = uint8(255);
+        current_cong([mid_2 bottom],[mid_2 bottom],2:3) = uint8(0);
+    end
+    
+    ax1= axes('Position',[0.015+(p-1).*0.19 0.52 0.2 0.2]);
+    image(ax1,current_cong);
+    axis square
+    title({['\delta = ', num2str(delta_1_x((i-1).*5 + p,1)), ', ', num2str(delta_1_x((i-1).*5 + p,3)), ' dva']});
+    set(gca,'FontName','Arial','FontSize',8,'FontWeight','normal','Box','off','XColor','none','YColor','none');
+    xticks([]),yticks([]);
+    
+    current_incong= imresize(imread(fullfile(folder2,theFiles2(delta_1_x((i-1).*5+p,2)).name)),[150 150]);
+    
+    if delta_1_x((i-1).*5+p,4) == 1
+        current_cong([top mid_1],[top mid_1],1) = uint8(255);
+        current_cong([top mid_1],[top mid_1],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 2
+        current_cong([top mid_1],[mid_1 mid_2],1) = uint8(255);
+        current_cong([top mid_1],[mid_1 mid_2],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 3
+        current_cong([top mid_1],[mid_2 bottom],1) = uint8(255);
+        current_cong([top mid_1],[mid_1 bottom],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 4
+        current_cong([mid_1 mid_2],[top mid_1],1) = uint8(255);
+        current_cong([mid_1 mid_2],[top mid_1],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 5
+        current_cong([mid_1 mid_2],[mid_1 mid_2],1) = uint8(255);
+        current_cong([mid_1 mid_2],[mid_1 mid_2],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 6
+        current_cong([mid_1 mid_2],[mid_2 bottom],1) = uint8(255);
+        current_cong([mid_1 mid_2],[mid_2 bottom],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 7
+        current_cong([mid_2 bottom],[top mid_1],1) = uint8(255);
+        current_cong([mid_2 bottom],[top mid_1],2:3) = uint8(0);
+    elseif delta_1_x((i-1).*5+p,4) == 8
+        current_cong([mid_2 bottom],[mid_1 mid_2],1) = uint8(255);
+        current_cong([mid_2 bottom],[mid_1 mid_2],2:3) = uint8(0);
+    else
+        current_cong([mid_2 bottom],[mid_2 bottom],1) = uint8(255);
+        current_cong([mid_2 bottom],[mid_2 bottom],2:3) = uint8(0);
+    end
+    ax2= axes('Position',[0.015+(p-1).*0.19 0.3 0.2 0.2]);
+    image(ax2,current_incong);
+    axis square
+    set(gca,'FontName','Arial','FontSize',8,'FontWeight','normal','Box','off','XColor','none','YColor','none');
+    xticks([]),yticks([]);
+
+end
+
+%     if i < 10
+%         filename = ['cong_0', num2str(i),'.jpg'];
+%     else
+%         filename = ['cong_', num2str(i), '.jpg'];
+%     end
+%     saveas(gcf,filename);
+    clf;
+end
+
+%%
+%incongruent
+for i = 1:16
+
+for p = 1:5
+    current_cong= imresize(imread(fullfile(folder1,theFiles1(delta_2_x((i-1).*5+p,2)).name)),[150 150]);
+    ax1= axes('Position',[0.015+(p-1).*0.19 0.52 0.2 0.2]);
+    image(ax1,current_cong);
+    axis square
+    title({['delta = ', num2str(delta_2_x((i-1).*5 + p,1)), ', ', num2str(delta_2_x((i-1).*5 + p,3)), ' dva']});
+    set(gca,'FontName','Arial','FontSize',8,'FontWeight','normal','Box','off','XColor','none','YColor','none');
+    xticks([]),yticks([]);
+    
+    current_incong= imresize(imread(fullfile(folder2,theFiles2(delta_2_x((i-1).*5+p,2)).name)),[150 150]);
+    ax2= axes('Position',[0.015+(p-1).*0.19 0.3 0.2 0.2]);
+    image(ax2,current_incong);
+    axis square
+    set(gca,'FontName','Arial','FontSize',8,'FontWeight','normal','Box','off','XColor','none','YColor','none');
+    xticks([]),yticks([]);
+
+end
+
+    if i < 10
+        filename = ['incong_0', num2str(i),'.jpg'];
+    else
+        filename = ['incong_', num2str(i), '.jpg'];
+    end
+    saveas(gcf,filename);
+    clf;
+end
+
+%% create scatterplot
+[A,img_num1] = sort(delta_1_x(:,2));
+[B,img_num2] = sort(delta_2_x(:,2));
+
+scatter(delta_1_x(img_num1,1),delta_2_x(img_num2,1),'filled');
+xlabel(['\delta' 'rcDxC in congruent condition']), ylabel(['\delta' 'rcDxC in incongruent condition']);
 set(gca,'FontName','Arial','FontSize',12);
-xlim([-7 7]);
-box off
+
+
 
