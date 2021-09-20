26f7b437174c4a13f8ee789452cb955596cc5cde
lqchel <48194751+lqchel@users.noreply.github.com>
2021-09-20 15:51:26 +1000

    Create get_data2.m
    this code loads experiment 2 data and relevant trial indices

---
+++ get_data2.m
@@ -0,0 +1,23 @@
+%%% this code loads the Exp 2 data and trial indexes
+
+Results = importdata('Exp2_data.mat');
+Results = Results(Results(:,11)~=0,:);
+Results(:,9) = Results(:,8).*Results(:,9);
+
+%Find trials that presented N patches -- signal absent for hypo 1
+%Find trials that presented N patches -- signal absent for hypo 1
+Find_N = Results(:,5) ==1;
+
+% present patch trials -- signal present for hypo 1
+Find_CAP = Results(:,4) == 0 & Results(:,5) == 2; 
+Find_IAP = Results(:,4) == 1 & Results(:,5) == 3; 
+
+%Congruent trial with Congruent object, and incongruent trial with
+%incongruent object -- signal present for hypo 2
+Find_Congruent_CP = Results(:,4) == 0 & Results(:,5) == 2 & Results(:,6) == 1; %% Results(:,6) == 1 for exp 2
+Find_Incongruent_IP = Results(:,4) == 1 & Results(:,5) == 3 & Results(:,6) == 1;
+
+%Incongruent trial with congruent object, congruent trial with incongruent
+%object -- signal absent for hypo 2
+Find_Congruent_IP = Results(:,4) == 0 & Results(:,5) == 3 & Results(:,6) == 1;
+Find_Incongruent_CP = Results(:,4) == 1 & Results(:,5) == 2 & Results(:,6) == 1;
